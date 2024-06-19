c660cc375bb5b720d33ec697cf62ef9d732e62a8
Henry García <henrygarciaospina@gmail.com>
2024-06-19 10:17:26 -0500

    Initial configuration and definition of some functionalities.

---
+++ BlazorMusicCatalog.Data/Migrations/20240619051503_AddNumberToSongs.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorMusicCatalog.Data.Migrations
+{
+    public partial class AddNumberToSongs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Number",
+                table: "Song",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "Song");
+        }
+    }
+}
